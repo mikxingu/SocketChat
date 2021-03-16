69658ede19c8e1a5a9daab2ada71c57d8a78c3ef
Mike Alves <michelalvs@gmail.com>
2021-03-16 11:36:51 -0300

    Installed and Instantiated Socket.IO

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('express')(); //Initializes app
 const http = require('http').createServer(app); //Creates http server with app
+const io = require('socket.io')(http);
 
 
 //Handles requests at server homepage
@@ -7,6 +8,13 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+
+io.on('connection', (socket) => {
+    console.log('User Connected.');
+    socket.on('disconnect', () => {
+        console.log('User Disconnected.')
+    });
+});
 
 //Port used by the server.
 http.listen(3000, () => {
