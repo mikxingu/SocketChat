a2eea77f9153d2149c4a2842ba77afe888d412d2
Mike Alves <michelalvs@gmail.com>
2021-03-22 13:57:32 -0300

    Final implementation of the chat system

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-
 io.on('connection', (socket) => {
-    console.log('User Connected.');
-    socket.on('disconnect', () => {
-        console.log('User Disconnected.')
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
     });
 });
+
+// io.on('connection', (socket) => {
+
+//     socket.on('chat message', (msg) => {
+//         console.log('message: ' + msg);
+//     });
+//     //console.log('User Connected.');
+//     //socket.on('disconnect', () => {
+//     //  console.log('User Disconnected.')
+//     //});
+// });
 
 //Port used by the server.
 http.listen(3000, () => {
